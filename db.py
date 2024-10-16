d07cb979e7f5bc87d46d7182acb0f532da651674
LabAsim <lampgit@pm.me>
2024-10-16 13:49:22 +0300

    convert Db to context manager, modify tables

--- db.py
+++ db.py
@@ -1,26 +1,41 @@
+import logging
 import sqlite3
+from contextlib import ContextDecorator
 
 import pandas
 
+logger = logging.getLogger(__name__)
 
-class Db:
+
+class Db(ContextDecorator):
     """Manages the sqlite db"""
 
-    def __init__(self):
+    def __init__(self) -> None:
         self.conn = sqlite3.connect(database="coins.db")
         self.cursor = self.conn.cursor()
+        self.create_tables()
 
-    def create_tables(self):
+    def create_tables(self) -> None:
         """Creates the tables"""
         self.cursor.execute(
             """
             CREATE TABLE IF NOT EXISTS coins(
-                name TEXT NOT NULL,
+                id TEXT PRIMARY KEY,
+                name TEXT,
                 timestamp timestamp with time zone,
-                price FLOAT NOT NULL,
-                total_volume FLOAT,
-                marketcap  FLOAT
-            )    
+                price BIGFLOAT NOT NULL,
+                marketcap  FLOAT,
+                total_volume FLOAT
+            );    
+            """
+        )
+        self.conn.commit()
+
+        self.cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS saved_coins(
+                name TEXT PRIMARY KEY
+            );    
             """
         )
         self.conn.commit()
@@ -29,4 +44,32 @@ class Db:
         """
         Inserts the prices etc. to the sqlite db.
         """
-        df.to_sql(name=coin, con=self.conn, index=False, if_exists="append")
+        query = f'''
+            INSERT OR REPLACE INTO coins(name,timestamp, price, total_volume, marketcap,id) values (?,?,?,?,?,?)
+            ON CONFLICT(id) DO NOTHING;
+        '''
+        df['id'] = df["name"] + df["Timestamp"]
+        logging.debug(f"{df.to_records(index=False)=}")
+        self.conn.executemany(query, df.to_records(index=False))
+        self.conn.commit()
+
+    def add_coins(self, coin: str) -> None:
+        """Adds the coin name to the db"""
+        logger.debug(f"{coin=}")
+        self.conn.execute(
+            '''
+            INSERT INTO saved_coins(name) values(?)
+            ON CONFLICT(name) DO NOTHING;
+            ''',
+            [coin]
+        )
+        self.conn.commit()
+
+    def __enter__(self):
+        """https://stackoverflow.com/a/42623484"""
+        logger.debug("Entering")
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.conn.close()
+        logger.debug("Connection closed")
